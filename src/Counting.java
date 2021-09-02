bca9e2c7a6e92f719655fae18ca648fca9833413
Amitaf1 <70072602+Amitaf1@users.noreply.github.com>
2021-09-03 01:17:40 +0200

    comments addad

--- src/Counting.java
+++ src/Counting.java
@@ -6,12 +6,26 @@ public class Counting {
 
         System.out.println("Teller til ti");
 
+        /*
+
+        Starts from 1 and adds on one number each time
+        until it reaches 10
+
+        */
+
         for (int i = 1; i <= 10; i++) {
 
             System.out.println(i);
         }
 
         System.out.println("Petra teller til tjue");
+
+        /*
+
+        Starts from 11 and adds on one number each time
+        until it reaches 20
+
+        */
 
         for (int i = 11; i <= 20; i++) {
 
