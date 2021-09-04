73d4129537e7876e67dac7cc4470e353785c1a6b
Amitaf1 <70072602+Amitaf1@users.noreply.github.com>
2021-09-05 00:32:42 +0200

    Added a few more projects

---
+++ src/exercises/week1/section113/Task6.java
@@ -0,0 +1,9 @@
+package exercises.week1.section113;
+
+import static structure.Table.fak;
+
+public class Task6 {
+    public static void main(String[] args) {
+        System.out.println(fak(3));
+    }
+}
