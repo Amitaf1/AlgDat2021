eb532f49bb94493eb94da6358e91953d4bde15b5
Amitaf1 <70072602+Amitaf1@users.noreply.github.com>
2021-09-12 00:39:37 +0200

    Modified 6. problem answer

--- src/tilOblig/AttemptA.java
+++ src/tilOblig/AttemptA.java
@@ -8,11 +8,11 @@ public class AttemptA {
 
         System.out.println(Arrays.toString(a));
 
-        rotasjon(a, 1);
+        rotasjon(a, 3);
 
         System.out.println(Arrays.toString(a));
 
-        rotasjon(a, -1);
+        rotasjon(a, -2);
 
         System.out.println(Arrays.toString(a));
 
@@ -31,25 +31,39 @@ public class AttemptA {
         for (int i = 0; i < a.length; i++) {
 
             // Denne sjekker om lengden av arrayen "a" er større enn hvor mange enheter hver bokstav skal forflytte seg. Om lengden til "a" er større en dette tallet, så vil ikke resten av for-loopen kjøre
-            if (a.length > k) {
+            if (a.length <= k || a.length <= -k) {
 
                 // "break;" gjør at, i stedet for at neste del av koden utenfor "if"-blokken kjører, så vil det gjøre at hele for-loopen blir ferdig
                 break;
             }
 
-            // Siden "i" starter på 0, så utføres koden inni her bare første gang for-loopen utføres
-            if (i >= 1) {
+            // Denne delen skal bare utføres om "k" er større enn 0
+            // Den skal også bare utføres mens "i" er mindre enn "k"
+            if (k > 0 && i < k) {
 
-                // Denne skifter den første elementet i "a"-arrayen (fordi i skal være 0 for at koden her skal kjøre) til den siste elementet i "a"-arrayen, dette skifter - i denne tilfellen - den første elementet i "a"-arrayen fra 'A' til 'I'
-                a[i] = a[a.length - 1];
+                // Denne skifter den første elementet i "a"-arrayen (fordi "k" skal være større enn 0 for at koden her skal kjøre) til den siste elementet i "a"-arrayen, dette skifter - i denne tilfellen - den første elementet i "a"-arrayen fra 'A' til 'I'
+                a[i] = a[a.length - k + i];
 
                 // "continue;" gjør at, i stedet for at neste del av koden utenfor "if"-blokken kjører, så vil det gjøre at for-loopen tror den er ferdig, og kjører igjen
                 continue;
             }
 
+            // Denne delen skal bare utføres om "k" er mindre enn 0
+            // Den skal også bare utføres mens "i" er stor nok for at de siste elementene som skal byttes ut (de siste to elementene i arrayen om "k" er -2), skal byttes ut fra den andre siden
+            if (k < 0 && i > a.length + k - 1) {
+
+                // Denne skifter den de siste elementene i "a"-arrayen (fordi "k" skal være mindre enn 0 for at koden her skal kjøre) til dee første elementet i "a"-arrayen
+                // Et eksempel: {}
+                a[i] -= a.length + k;
+
+                // "continue;" gjør at, i stedet for at neste del av koden utenfor "if"-blokken kjører, så vil det gjøre at for-loopen tror den er ferdig, og kjører igjen
+                continue;
+            }
+
+
             // "a[i] -= 1" gjør at elementet med indeksen "i" i arrayen "a" blir til elementet med én lavere indeks enn den.
             // Et eksempel: {'A', 'B', 'C', 'D'} --> {'A', 'A', 'C', 'D'} --> {'A', 'A', 'B', 'D'} --> ...
-            a[i] -= 1;
+            a[i] -= k;
         }
 
     }
