effd92a381c5458641551b7a74bb77b6ae0a761f
Amitaf1 <70072602+Amitaf1@users.noreply.github.com>
2021-09-16 23:42:05 +0200

    Fixed oppgave 4 and added additional comments

--- src/exercises/week2/Bubblesort2.java
+++ src/exercises/week2/Bubblesort2.java
@@ -28,7 +28,10 @@ public class Bubblesort2 {
         System.out.println("Fant tallet 11 rekursivt på " + index4);
     }
 
-    public static int binarySearchRecursive(int[] values, int search_value, int l, int r) {
+    public static int binarySearchRecursive(int[] values,
+                                            int search_value,
+                                            int l,
+                                            int r) {
         int m = (l+r) / 2;
 
         if (l > r) {
@@ -54,7 +57,107 @@ public class Bubblesort2 {
     }
 
 
+    /*
+    Binærsøk
+    //Intervaller:
+    // Halvåpent intervall:
+    // [8, 10) => 8 9 => vanlig for-løkke: for(int i=8; i<10; ++i)
+    // (8, 10] => 9, 10
+    // Lukket intervall:
+    // [8, 10] => 8, 9, 10 => "vanlig" for-løkke: for(int i=8; i<=10; ++i)
+    //Søk etter 6
+    //[ l                 r ]
+    //[ a b c d e f g h i j ]
+    //[ 0 1 2 3 4 5 6 7 8 9 ]
+    // values.length = 10
+    // l = 0 //left index 0
+    // r = 9 //right index 9
+    // m = (l+r) / 2 = (0 + 9) / 2 = 4 //middle
+    //Tanke: (0.0 + 9.0) / 2 // OBS: Double promotering
+    //Tegneserie
+    //1
+    //[ l       m         r ]
+    //[ a b c d 9 f g h i j ]
+    //[ 0 1 2 3 4 5 6 7 8 9 ]
+    //2
+    //[ l m   r             ]
+    //[ a 2 c d 9 f g h i j ]
+    //[ 0 1 2 3 4 5 6 7 8 9 ]
+    //3
+    //[     lm r             ]
+    //[ a 2 5 d 9 f g h i j ]
+    //[ 0 1 2 3 4 5 6 7 8 9 ]
+    // (l+r)/2 => (3+2)/2 = 5/2 = 2
+    //4
+    //[       lr            ]
+    //[ a 2 5 7 9 f g h i j ]
+    //[ 0 1 2 3 4 5 6 7 8 9 ]
+    // Fant tallet på 4 søk.
+    */
     public static int binarySearch(int[] values, int search_value) {
+        //Søk i intervallet [l, r]:
+        int l = 0;
+        int r = values.length - 1;
+        int m = (l+r)/2;
+
+        while (l <= r) {
+            System.out.println("Sjekker intervallet [" + l + ", " + r + "]");
+            m = (l+r)/2;
+            //Hvis midt-verdien er større,
+            //gå til venstre
+            //[ l       m         r ]
+            //[ a b c d 9 f g h i j ]
+            if (values[m] > search_value) {
+                //[ l     r             ]
+                //[ a b c d 9 f g h i j ]
+                r = m - 1;
+            } else if (values[m] < search_value) {
+                //[           l       r ]
+                //[ a b c d 9 f g h i j ]
+                l = m + 1;
+            } else if (values[m] == search_value) {
+                //Vi har funnet plassen til verdien.
+                //eller NaN (f.eks. 9/0)
+                return m;
+            } else {
+                System.out.println("Something wrong!");
+            }
+        }
+
+        //Fant ikke verdien...
+        return -l;
+    }
+
+    /**
+     * Sorterer verdien in-place (OBS: rekkefølgen i values blir endret!)
+     * @param values
+     */
+    public static void bubbleSort(int[] values) {
+        //To for-løkker:
+        // Ytre løkke: sorterer ett tall per gjennomgang
+        for (int i=0; i < values.length - 1; ++i) {
+            System.out.println("Array før sortering av index " + i);
+            System.out.println(Arrays.toString(values));
+            // indre løkke: "bobler opp" tall til riktig posisjon
+            for (int j=values.length-1; j > i; j--) {
+                //Sjekk om tall på posisjon j og (j-1) er sortert riktig
+                if (values[j] < values[j-1]) {
+                    System.out.println("Inversjon! Bytter plass på " + j + " og " + (j-1) );
+                    int tmp = values[j];
+                    values[j] = values[j-1];
+                    values[j-1] = tmp;
+                    System.out.println(Arrays.toString(values));
+                }
+            }
+        }
+
+        System.out.println("Sorterte verdier:");
+        System.out.println(Arrays.toString(values));
+    }
+
+}
+
+
 
         // Intervaller:
         // Halvåpent intervall:
@@ -100,81 +203,3 @@ public class Bubblesort2 {
         // [ 0 1 2 3 4 5 6 7 8 9 ]
 
         // Søk i intervallet L, r;
-        int l = 0;
-        int r = values.length - 1;
-        int m = (l + r) / 2;
-
-        while (l <= r) {
-
-            System.out.println("Sjekker intervallet [" + l + ", " + r + "]");
-
-            m = (l + r) / 2;
-
-
-            // Hvis midt-verdien er større, gå til venstre
-            // [ L       m         r ]
-            // [ a b c d 9 f g h i j ]
-            if (values[m] > search_value) {
-
-                // [ L     r             ]
-                // [ a b c d 9 f g h i j ]
-
-                r = m - 1;
-            } else if (values[m] < search_value) {
-
-                // [           l       r ]
-                // [ a b c d 9 f g h i j ]
-
-                l = m + 1;
-            } else if (values[m] == search_value) {
-
-                // Vi har funnet plassen til verdien
-                // eller NaN (f.eks. 9/0 eller sqrt(-1))
-                return m;
-            } else {
-                System.out.println("Something wrong!");
-            }
-        }
-
-        //Fant ikke verdien...
-        return -l;
-    }
-
-    /**
-     * Sorterer verdien in-place (OBS: rekkefølgen i values blir endret!)
-     * @param values
-     */
-
-    public static void bubbleSort(int[] values) {
-
-        //To for-løkker:
-
-        // Ytre løkke: sorterer ett tall per gjennomgang
-
-        for (int i=0; i < values.length - 1; ++i) {
-
-            System.out.println("Array før sortering av index " + i);
-            System.out.println(Arrays.toString(values));
-
-            // indre løkke: "bobler opp" tall til riktig posisjon
-            for (int j=values.length-1; j > i; j--) {
-
-                //Sjekk om tall på posisjon j og (j-1) er sortert riktig
-                if (values[j] < values[j-1]) {
-
-                    System.out.println("Inversjon! Bytter plass på " + j + " og " + (j-1) );
-
-                    int tmp = values[j];
-                    values[j] = values[j-1];
-                    values[j-1] = tmp;
-
-                    System.out.println(Arrays.toString(values));
-                }
-            }
-        }
-
-        System.out.println("Sorterte verdier:");
-        System.out.println(Arrays.toString(values));
-    }
-
-}
--- src/tilOblig/AttemptA6.java
+++ src/tilOblig/AttemptA6.java
@@ -4,88 +4,160 @@ import java.util.Arrays;
 
 public class AttemptA6 {
 
+    public static void main(String[] args) {
+        int[] a = {6, 10, 9, 4, 1, 3, 8, 5, 2, 7};
+
+        delsortering(a);
+
+        System.out.println(Arrays.toString(a));
+    }
+
     ///// Oppgave 4 //////////////////////////////////////
     public static void delsortering(int[] a) {
 
+        // Lager en int som heter "n" dette skal være lengden til arrayen som vi begynte med
+        // Vi gjør dette for at koden skal ikke regne ut lengden til hoved-arrayen hver gang vil vil ha den
         int n = a.length;
 
+
+        // Dette gjør det slik at, om lengden av arrayen er lik null, så vil ikke koden kjøre
+        // Dette er for at ingen feil skal forekomme i koden
         if (n == 0) {
             return;
         }
 
-        int pLen = 0;
-        int oLen = 0;
+
+        // Disse to linjene finner ut indeksen til den første og siste tallet i hoved-arrayen
+        int a_først = 0;
+        int a_sist = n - 1;
 
 
+        // Dette skal sortere alle tallene
+        // Kommentarer er inni metodene nede
+        quickSort(a, n, a_først, a_sist);
+
+
+        // Lager to tall som skal representere lengden til arrayen som skal inneholde partall og oddetall
+        int partall_lengde = 0;
+        int oddetall_lengde = 0;
+
+
+        // En for loop som går igjennom hver element i arrayen
         for (int i : a) {
+
+            // Om tallet er partall, så skal tallet som skal bestemme lengden til arrayen for partall, øke med en
+            // Dette skjer for at partalls-arrayen skal ha muligheten til å romme alle partall
             if (i % 2 == 0) {
-                pLen += 1;
-            } else {
-                oLen += 1;
+                partall_lengde += 1;
+            }
+
+            // Om tallet er ikke partall, så vil den være oddetall
+            else {
+                oddetall_lengde += 1;
             }
         }
 
-        int[] partall = new int[pLen];
-        int[] oddetall = new int[oLen];
-        int delingP = 0;
-        int delingO = 0;
+        // Vi lager nå arrayen for partall og oddetall
+        // Lengden har vi hentet ut fra forrige for-loop
+        int[] partall = new int[partall_lengde];
+        int[] oddetall = new int[oddetall_lengde];
+
+        // Disse tallene skal hjelpe oss for at indeksen til tallene i partall og oddetall, ikke kommer ut feil
+        // Disse skal holde styr på hvor mange ganger vi har satt tall inn i hver av de to arrayene
+        int partall_ganger = 0;
+        int oddetall_ganger = 0;
 
         for (int i = 0; i < n; i++) {
             if (a[i] % 2 == 0) {
-                partall[i - delingO] = a[i];
-                delingP += 1;
+
+                // Om tallet er partall, så skal tallet kopieres inn i partalls-arrayen
+                /*
+                Vi tar "i - oddetall-ganger", for om vi hadde hatt bare "i" så vil koden
+                legge et partall på feil indeks i partalls-arrayen om et oddetall har blitt lagd inn før den
+                 */
+                partall[i - oddetall_ganger] = a[i];
+
+                // Etter at vi har lagd inn et partall, så vil partall_ganger øke
+                // Fordi da vil koden vite å trekke fre èn mer fra oddetall
+                partall_ganger += 1;
             }
-            else{
-                oddetall[i - delingP] = a[i];
-                delingO += 1;
+            else {
+                oddetall[i - partall_ganger] = a[i];
+                oddetall_ganger += 1;
             }
         }
 
-        int oLav = 0;
-        int oHøy = oLen - 1;
-        int pLav = 0;
-        int pHøy = pLen - 1;
 
-
-        quickSort(oddetall, oLav, oHøy);
-        quickSort(partall, pLav, pHøy);
-
-
-        for (int i = 0; i < oLen; i++) {
+        // For hver oddetall i oddetall-arrayen, så vil den erstatte det tallet fra hoved-arrayet med oddetallet
+        for (int i = 0; i < oddetall_lengde; i++) {
             a[i] = oddetall[i];
         }
 
-        for (int i = 0; i < pLen; i++) {
-            a[i + oLen] = partall[i];
+        /*
+        Lengden til oddetalls-arrayenindeksen til a-indeksen, fordi alle oddetall har blitt lagd til
+        a-arrayen allerede
+        */
+        for (int i = 0; i < partall_lengde; i++) {
+            a[i + oddetall_lengde] = partall[i];
         }
     }
 
-    public static int partisjon(int[] a, int lav, int høy) {
-        int mellom = a[høy];
-        int i = lav - 1;
+    // Lager en metode som skal holde styr på partisjonene
+    public static int partisjon(int[] a, int n, int først, int sist) {
 
-        for (int j = lav; j <= høy; j++) {
-            if (a[j] < mellom) {
+        // Dette velger den siste elementet i a-arrayen som partisjon
+        int partisjon_tall = a[sist];
+
+
+        // Dette gjør klar for at den første elementet skal velges
+        // Om man bare gjør "i = først", så vil det ende opp i at koden kjører seg uendelig ganger
+        int i = først - 1;
+
+        // Denne delen av koden kjører en gang for hver at tallene i a-arrayen
+        for (int j = først; j <= sist; j++) {
+
+            /*
+            Denne delen av koden bare kjører om elementet (teller fra null) i a-arrayen er mindre enn tallet
+            som er partisjonstallet (for å sammenligne)
+            */
+            if (a[j] < partisjon_tall) {
                 i++;
+
+                /*
+                Denne delen av koden minker indeksen til et element i a-arrayen, dette skjer ved
+                å lage en midlertidig kopi av den elementet som brukes for å bytte elementets posisjon
+                med elementet med indeks i
+                Eksempel: "1, 4, 5, 2, 5" --> "1, 2, 5, 4, 3"
+                */
                 int temp = a[i];
                 a[i] = a[j];
                 a[j] = temp;
             }
         }
 
+        // Denne delen skifter det første elementet som er større enn partisjonstallet, med partisjonstallet
         int temp = a[i + 1];
-        a[i + 1] = a[høy];
-        a[høy] = temp;
+        a[i + 1] = a[sist];
+        a[sist] = temp;
 
+        // Dette gir tilbake tallet som er partisjonstallet
         return (i + 1);
     }
 
-    public static void quickSort(int[] array, int lav, int høy) {
-        if (lav < høy) {
-            int mellom = partisjon(array, lav, høy);
+    // Selve sorteringsmetoden
+    public static void quickSort(int[] a, int n, int først, int sist) {
+        if (først < sist) {
 
-            quickSort(array, lav, mellom - 1);
-            quickSort(array, mellom + 1, høy);
+            // Indeks på partisjonen
+            int partisjon_indeks = partisjon(a, n, først, sist);
+
+            // Disse to linjene lager nye partisjoner inni de to partisjonene som ble lagd
+            // En for alle tall som er større enn partisjonstallet
+            // En for alle tall som er mindre enn partisjonstallet
+            // Dette fortsetter helt til hver partisjon er èn element lang
+            // Deretter skal partisjonene bygges opp igjen
+            quickSort(a, n, først, partisjon_indeks - 1);
+            quickSort(a, n, partisjon_indeks + 1, sist);
         }
     }
 
@@ -150,29 +222,41 @@ public class AttemptA6 {
     ///// Oppgave 6 //////////////////////////////////////
     public static void rotasjon(char[] a, int k) {
 
+        // Lager en int som heter "n" dette skal være lengden til arrayen som vi begynte med
+        // Vi gjør dette for at koden skal ikke regne ut lengden til hoved-arrayen hver gang vil vil ha den
         int n = a.length;
 
+
+        // Dette gjør det slik at, om lengden av arrayen er mindre enn en, så vil ikke koden kjøre
+        // Dette er for at ingen feil skal forekomme i koden
         if (n <= 1) {
             return;
         }
 
-        char[] savedElements = new char[Math.abs(k)];
+        // Dette skal lagre de elementene som skal lagres for senere bruk (den er nå tom)
+        char[] saved_elements = new char[Math.abs(k)];
 
         if (k > 0) {
+            // Gjør at forskyvelsen blir mer effektiv, ved å trekke fra lengden til a
+            // Eksempel: Om lengden til a er 10, så vil det å forskyve 101 elementer være det samme
+            // som å forskyve 1 element
             while (k > n - 1) {
-                    k -= n;
+                k -= n;
             }
 
-            for (int iSave = k; iSave > 0; iSave--) {
-                savedElements[k - iSave] = a[n - iSave];
+            // Lagrer de elementene som må lagres (antall elementer som forskyves)
+            for (int i = k; i > 0; i--) {
+                saved_elements[k - i] = a[n - i];
             }
 
-            for (int iFirst = n - 1; iFirst >= k; iFirst--) {
-                a[iFirst] = a[iFirst - k];
+            // Forskyver elementene
+            for (int j = n - 1; j >= k; j--) {
+                a[j] = a[j - k];
             }
 
-            for (int iLast = 0; iLast < Math.abs(k); iLast++) {
-                a[iLast] = savedElements[iLast];
+            // Setter inn de lagrede elementene i starten
+            for (int l = 0; l < Math.abs(k); l++) {
+                a[l] = saved_elements[l];
             }
 
         }
@@ -182,16 +266,16 @@ public class AttemptA6 {
                 k += n;
             }
 
-            for (int iSave = 0; iSave < Math.abs(k); iSave++) {
-                savedElements[iSave] = a[iSave];
+            for (int i = 0; i < Math.abs(k); i++) {
+                saved_elements[i] = a[i];
             }
 
-            for (int iFirst = 0; iFirst < n + k; iFirst++) {
-                a[iFirst] = a[iFirst - k];
+            for (int j = 0; j < n + k; j++) {
+                a[j] = a[j - k];
             }
 
-            for (int iLast = 0; iLast < Math.abs(k); iLast++) {
-                a[n + iLast + k] = savedElements[iLast];
+            for (int l = 0; l < Math.abs(k); l++) {
+                a[n + l + k] = saved_elements[l];
             }
 
         }
